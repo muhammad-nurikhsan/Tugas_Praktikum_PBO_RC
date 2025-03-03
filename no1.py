8efef7ead7572da7b85177b45585cd2a66257f7e
Muhammad Nurikhsan <muhammad.123140057@student.itera.ac.id>
2025-03-04 01:32:29 +0700

    Delete no1.py

--- no1.py
+++
@@ -1,9 +0,0 @@
-# Muhammad Nurikhsan
-# 123140057
-# Praktikum PBO RC
-
-# No 1
-height = int (input("height: "))
-
-for i in range(1, height + 1):
-    print(((height - i + 1) * " ") + (i * "*") +((i - 1) * "*"))
