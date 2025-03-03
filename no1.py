6667eed076ab08ec711eae85edf77cd9c119ba6a
Muhammad Nurikhsan <muhammad.123140057@student.itera.ac.id>
2025-03-04 01:31:35 +0700

    Create no1.py

---
+++ no1.py
@@ -0,0 +1,9 @@
+# Muhammad Nurikhsan
+# 123140057
+# Praktikum PBO RC
+
+# No 1
+height = int (input("height: "))
+
+for i in range(1, height + 1):
+    print(((height - i + 1) * " ") + (i * "*") +((i - 1) * "*"))
